f5f24cdab1585b3ef658a30a329a7219eaeca1f3
Tang Rufus <tangrufus@gmail.com>
2018-09-01 07:45:15 +0800

    Update coding standards; Enable PrettyCI

--- command.php
+++ command.php
@@ -10,9 +10,8 @@ if (! class_exists('WP_CLI')) {
     return;
 }
 
-$autoload = __DIR__ . '/vendor/autoload.php';
-if (file_exists($autoload)) {
-    require_once $autoload;
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
 }
 
 WP_CLI::add_command('image-optimize', __NAMESPACE__ . '\ImageOptimizeCommand');
