e1eb0c7e5986ade0d98e62cf05ffc6fb3e41cf17
Tang Rufus <tangrufus@gmail.com>
2018-09-02 03:18:33 +0800

    Update test setup

--- tests/_support/Helper/Unit.php
+++ tests/_support/Helper/Unit.php
@@ -4,12 +4,23 @@ declare(strict_types=1);
 
 namespace TypistTech\ImageOptimizeCommand\Helper;
 
-use Codeception\Module;
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
 
-/**
- * Here you can define custom actions
- * all public methods declared in helper class will be available in $I
- */
+use Codeception\Module;
+use Codeception\TestInterface;
+use Mockery;
+use WP_Mock;
+
 class Unit extends Module
 {
+    public function _before(TestInterface $test)
+    {
+        WP_Mock::setUp();
+    }
+    public function _after(TestInterface $test)
+    {
+        WP_Mock::tearDown();
+        Mockery::close();
+    }
 }
--- tests/_support/UnitTester.php
+++ tests/_support/UnitTester.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace TypistTech\ImageOptimizeCommand;
 
+use Codeception\Actor;
+
 /**
  * Inherited Methods
- *
  * @method void wantToTest($text)
  * @method void wantTo($text)
  * @method void execute($callable)
@@ -16,15 +17,15 @@ namespace TypistTech\ImageOptimizeCommand;
  * @method void am($role)
  * @method void lookForwardTo($achieveValue)
  * @method void comment($description)
- * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = null)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
  *
  * @SuppressWarnings(PHPMD)
  */
-class UnitTester extends \Codeception\Actor
+class UnitTester extends Actor
 {
     use _generated\UnitTesterActions;
 
-    /*
-     * Define custom actions here
-     */
+   /**
+    * Define custom actions here
+    */
 }
