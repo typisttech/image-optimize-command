1d8a78704a2ffe80b6dfcbc62e061d8907a166aa
Tang Rufus <tangrufus@gmail.com>
2018-09-03 06:12:45 +0800

    Apply code style

--- tests/unit/CLI/LoggerFactoryTest.php
+++ tests/unit/CLI/LoggerFactoryTest.php
@@ -18,11 +18,11 @@ class LoggerFactoryTest extends Unit
     public function testCreate()
     {
         WP_Mock::userFunction(__NAMESPACE__ . '\apply_filters')
-            ->with('typist_tech_image_optimized_logger', Mockery::type(Logger::class))
-            ->andReturnUsing(function ($_hook, $arg) {
-                return $arg;
-            })
-            ->once();
+               ->with('typist_tech_image_optimized_logger', Mockery::type(Logger::class))
+               ->andReturnUsing(function ($_hook, $arg) {
+                   return $arg;
+               })
+               ->once();
 
         $actual = LoggerFactory::create();
 
